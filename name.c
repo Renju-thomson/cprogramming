6886f25bc1582fb630372c6c19c531a70afd1a82
Renju-thomson <62929150+Renju-thomson@users.noreply.github.com>
2020-04-01 09:15:35 +0530

    first commit

---
+++ name.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Name:Renju Thomson\n");
+    printf("Department:Electronics and Communication\n");
+    printf("College:College of Engineering,Chenganur\n");
+    return 0;
+}
