ae9536c128ba0dcfb504c26ff3b6d240b8452b46
Renju-thomson <62929150+Renju-thomson@users.noreply.github.com>
2020-04-02 10:34:19 +0530

    commit 2

---
+++ data.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char NAME[]="Susan";
+    char FOODS[]="milk,biscuits";
+    int AGE=4;
+
+    printf("Petname=%s\n", NAME);
+    printf("Age=%d\n", AGE);
+    printf("Fav Foods=%s\n", FOODS);
+    return 0;
+}
---
+++ text.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int studentage=14;
+    int studentclass=9;
+    double studentmark=90.2;
+    char studentname[]="Manju";
+    char studentschool[]="St.Kuriakose Public School";
+    printf("Name=%s\n",studentname);
+    printf("School=%s\n",studentschool);
+    printf("Age=%d\n",studentage);
+    printf("CLass=%d\n",studentclass);
+    printf("Marks=%f\n",studentmark);
+    return 0;
+}
